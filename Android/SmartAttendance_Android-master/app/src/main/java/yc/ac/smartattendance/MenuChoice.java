3c3ef5da8fd77ec485d084aa5e0982a57666873a
terry-koo <21760003@st.yc.ac.kr>
2021-05-27 01:50:16 +0900

    디자인 수정
    연암공대 로고 삽입/액션바 추가

--- Android/SmartAttendance_Android-master/app/src/main/java/yc/ac/smartattendance/MenuChoice.java
+++ Android/SmartAttendance_Android-master/app/src/main/java/yc/ac/smartattendance/MenuChoice.java
@@ -18,6 +18,11 @@ public class MenuChoice extends AppCompatActivity {
     private Button open; // 출석 열기 버튼
     private Button search; //  출석 조회 버튼
 
+    // 마지막으로 뒤로가기 버튼을 눌렀던 시간 저장
+    private long backKeyPressedTime = 0;
+    // 첫 번째 뒤로가기 버튼을 누를때 표시
+    private Toast toast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -81,4 +86,29 @@ public class MenuChoice extends AppCompatActivity {
             });
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        // 기존 뒤로가기 버튼의 기능을 막기위해 주석처리 또는 삭제
+        // super.onBackPressed();
+
+        // 마지막으로 뒤로가기 버튼을 눌렀던 시간에 2초를 더해 현재시간과 비교 후
+        // 마지막으로 뒤로가기 버튼을 눌렀던 시간이 2초가 지났으면 Toast Show
+        // 2000 milliseconds = 2 seconds
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis();
+            toast = Toast.makeText(this, "\'뒤로\' 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        // 마지막으로 뒤로가기 버튼을 눌렀던 시간에 2초를 더해 현재시간과 비교 후
+        // 마지막으로 뒤로가기 버튼을 눌렀던 시간이 2초가 지나지 않았으면 종료
+        // 현재 표시된 Toast 취소
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            finish();
+            finishAffinity();
+            toast.cancel();
+        }
+    }
+
 }
