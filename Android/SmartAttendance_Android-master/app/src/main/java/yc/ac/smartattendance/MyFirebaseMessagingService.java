165f56b03cbc63c7f3b6d71259d5145b7e3ef2c9
terry-koo <21760003@st.yc.ac.kr>
2021-05-26 13:49:37 +0900

    클라우드 메세지 분기

--- Android/SmartAttendance_Android-master/app/src/main/java/yc/ac/smartattendance/MyFirebaseMessagingService.java
+++ Android/SmartAttendance_Android-master/app/src/main/java/yc/ac/smartattendance/MyFirebaseMessagingService.java
@@ -62,7 +62,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService  {
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.drawable.ic_baseline_favorite_24)
+                        .setSmallIcon(R.drawable.yonam)
                         .setContentTitle(messageTitle)
                         .setContentText(messageBody)
                         .setAutoCancel(true)
