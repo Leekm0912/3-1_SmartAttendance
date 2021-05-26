5c6fc5439762ee322f4d83dde3b790b9371c0c92
terry-koo <21760003@st.yc.ac.kr>
2021-05-27 02:04:46 +0900

    디자인 개선
    액션바 일부 제거/위젯 위치 조정

--- Android/SmartAttendance_Android-master/app/src/main/java/yc/ac/smartattendance/Login.java
+++ Android/SmartAttendance_Android-master/app/src/main/java/yc/ac/smartattendance/Login.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -34,6 +35,7 @@ public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
